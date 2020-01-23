bc6318b4eafabae1c7b3672dfcd5ae72a13c6adf
Parsa <parsasi@rocketmail.com>
2020-01-23 10:50:20 -0800

    working on fetch

--- script.js
+++ script.js
@@ -9,29 +9,31 @@ const report = (celsius, fahrenheit) => {
 document.querySelector('#temp-form').addEventListener('submit', (e) => {
     e.preventDefault();
     const value = document.querySelector('#temp-input').value.toLowerCase();
+
+    
+    let response = fetch('http://localhost:8080/data', {
+        method: 'POST', 
+        encoding: 'utf-8',
+        mode: 'no-cors',
+        headers: { 'content-type': 'text/plain' },
+        body: {temp : convertValue(value)}
+    })
+    .then(response => response.json())
+    .then(jsonData => document.querySelector('body').innerHTML += jsonData.message)
+    .catch(err => document.querySelector('body').innerHTML += err); 
+});
+
+const convertValue = (value) => {
     const num = parseInt(value);
     console.log(value);
     console.log(num);
-
     if (value.includes("c")) {
         report(num, Math.round(1.8 * num + 32));
+        return Math.round(1.8 * num + 32);
     } else if (value.includes("f")) {
         report(Math.round((num - 32) / 1.8), num);
+        return Math.round((num - 32) / 1.8);
     } else {
         alert("Invalid input. Please input the temperature followed by unit letter. Ex. 23C, 78F");
     }
-
-    /* 
-        //TODO: Validate Values
-        //TODO: Convert Values
-        let response = fetch('http://localhost:3000', {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            encoding: 'utf-8',
-            mode: 'no-cors',
-            headers: { 'content-type': 'text/plain' },
-            body: value
-        })
-            .then(response => response.json())
-            .then(jsonData => document.querySelector('body').innerHTML += jsonData.message)
-            .catch(err => document.querySelector('body').innerHTML += err); */
-});
+}
